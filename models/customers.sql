29a8cdfe4a2334b4a9821f54999348ba51a7bc49
Cynthia Zinsou <mayazins44@gmail.com>
2026-07-02 13:50:21 +0000

    updated

--- models/customers.sql
+++ models/customers.sql
@@ -1,14 +1,12 @@
 
 with customers as (
-
---    select * from {{ ref('stg_jaffle_shop__customers')}}
-select * from analytics.dbt_czinsou.stg_jaffle_shop__customers
+   select * from {{ ref('stg_jaffle_shop__customers')}}
+-- select * from analytics.dbt_czinsou.stg_jaffle_shop__customers
 ),
 
 orders as (
-select * from analytics.dbt_czinsou.stg_jaffle_shop__orders
-
---    select * from {{ ref('stg_jaffle_shop__orders')}}
+--select * from analytics.dbt_czinsou.stg_jaffle_shop__orders
+   select * from {{ ref('stg_jaffle_shop__orders')}}
 ),
 
 customer_orders as (
